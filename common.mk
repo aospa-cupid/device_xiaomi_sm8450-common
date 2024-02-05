5a44025a529ce595268d3499352a968c214bcf4f
Sugakesshaa <sugakesshaa@pixelos.net>
2024-02-05 22:37:06 +0800

    sm8450-common: Decommonize sensor HAL and ParanoidDoze
    Change-Id: Icfcfc31cf9df9a7bc47566b99048de0c7c5b41c6

--- common.mk
+++ common.mk
@@ -156,15 +156,6 @@ PRODUCT_VENDOR_PROPERTIES += \
     vendor.display.enable_hist_intr=1 \
     vendor.display.idle_time=0
 
-# Doze
-PRODUCT_PACKAGES += \
-    ParanoidDoze
-
-PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    ro.sensor.pickup=xiaomi.sensor.pickup \
-    ro.sensor.pickup.lower.value=2 \
-    ro.sensor.proximity=true
-
 # DPM
 PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.dpm.vndr.feature=1 \
@@ -377,7 +368,6 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.multihal \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
